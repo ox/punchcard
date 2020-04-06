fe150224264851739054528f7a514332980643b7
Artem Titoulenko <artem.titoulenko@gmail.com>
2020-04-06 13:57:38 -0400

    go fmt errthang

--- card.go
+++ card.go
@@ -1,139 +1,139 @@
 package punchcard
 
 import (
-  "bytes"
-  "errors"
-  "fmt"
-  "io"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
 )
 
 type card struct {
-  encoding Encoding
-  content *bytes.Buffer
+	encoding Encoding
+	content  *bytes.Buffer
 }
 
 func (c *card) Bytes() []byte {
-  return c.content.Bytes()
+	return c.content.Bytes()
 }
 
 func New(encoding Encoding, contents *bytes.Buffer) (*card, error) {
-  body := bytes.NewBufferString("H80")
+	body := bytes.NewBufferString("H80")
 
-  lines := make([][]byte, 0)
+	lines := make([][]byte, 0)
 
-  for ; contents.Len() > 0; {
-    line := make([]byte, 80)
+	for contents.Len() > 0 {
+		line := make([]byte, 80)
 
-    for i := 0; i < 80; {
-      byte, err := contents.ReadByte()
-      if err == io.EOF {
-        break
-      }
+		for i := 0; i < 80; {
+			byte, err := contents.ReadByte()
+			if err == io.EOF {
+				break
+			}
 
-      // blank out the rest of the card if we find a newline
-      if byte == '\n' {
-        for ; i < 80; i++ {
-          line[i] = ' '
-        }
-      } else if byte == '\t' {
-        for k := i; k < 80 && k < i + 4; k++ {
-          line[i] = ' '
-        }
-        i+=4
-      } else {
-        line[i] = byte
-        i++
-      }
-    }
+			// blank out the rest of the card if we find a newline
+			if byte == '\n' {
+				for ; i < 80; i++ {
+					line[i] = ' '
+				}
+			} else if byte == '\t' {
+				for k := i; k < 80 && k < i+4; k++ {
+					line[i] = ' '
+				}
+				i += 4
+			} else {
+				line[i] = byte
+				i++
+			}
+		}
 
-    lines = append(lines, line)
-  }
+		lines = append(lines, line)
+	}
 
-  encodingTable := GetEncodingTable(encoding)
-  if len(encodingTable) == 0 {
-    return nil, fmt.Errorf("Could not get encoding table '%s'", encoding)
-  }
+	encodingTable := GetEncodingTable(encoding)
+	if len(encodingTable) == 0 {
+		return nil, fmt.Errorf("Could not get encoding table '%s'", encoding)
+	}
 
-  for _, line := range lines {
-    body.Write([]byte{0x80,0x80,0x80})
+	for _, line := range lines {
+		body.Write([]byte{0x80, 0x80, 0x80})
 
-    for i := 0; i < len(line); {
-      even := byte(line[i])
-      odd := byte(line[i+1])
-      i += 2
+		for i := 0; i < len(line); {
+			even := byte(line[i])
+			odd := byte(line[i+1])
+			i += 2
 
-      evenCol := encodingTable[even]
-      oddCol := encodingTable[odd]
+			evenCol := encodingTable[even]
+			oddCol := encodingTable[odd]
 
-      a := evenCol >> 4
-      b := ((evenCol & 017) << 4) | (oddCol >> 8)
-      c := oddCol & 00377
+			a := evenCol >> 4
+			b := ((evenCol & 017) << 4) | (oddCol >> 8)
+			c := oddCol & 00377
 
-      body.Write([]byte{byte(a), byte(b), byte(c)})
-    }
-  }
+			body.Write([]byte{byte(a), byte(b), byte(c)})
+		}
+	}
 
-  return &card{encoding, body}, nil
+	return &card{encoding, body}, nil
 }
 
 func Read(card []byte, encoding Encoding) (*bytes.Buffer, error) {
-  // verify that it has the H80/H82 header format and skip over it
-  format := 80
-  endCol := 80
+	// verify that it has the H80/H82 header format and skip over it
+	format := 80
+	endCol := 80
 
-  if bytes.Equal(card[0:3], []byte("H80")) {
+	if bytes.Equal(card[0:3], []byte("H80")) {
 
-  } else if bytes.Equal(card[0:3], []byte("H82")) {
-    endCol = 82
-    format = 82
-  } else {
-    return nil, errors.New("Invalid card, missing valid header")
-  }
+	} else if bytes.Equal(card[0:3], []byte("H82")) {
+		endCol = 82
+		format = 82
+	} else {
+		return nil, errors.New("Invalid card, missing valid header")
+	}
 
-  buff := bytes.NewBuffer(card[3:])
-  ascii_code := GetDecodingTable(encoding)
-  text := bytes.NewBuffer([]byte{})
+	buff := bytes.NewBuffer(card[3:])
+	ascii_code := GetDecodingTable(encoding)
+	text := bytes.NewBuffer([]byte{})
 
-  for {
-    // First 3 bytes are the card metadata
-    metadata := make([]byte, 3)
-    if _, err := buff.Read(metadata); err == io.EOF {
-      return text, nil
-    }
+	for {
+		// First 3 bytes are the card metadata
+		metadata := make([]byte, 3)
+		if _, err := buff.Read(metadata); err == io.EOF {
+			return text, nil
+		}
 
-    line := bytes.NewBufferString("")
+		line := bytes.NewBufferString("")
 
-    // Read `format` columns from the card body
-    for i := 0; i < endCol; {
-      // Get 3 bytes, and cast them to uint32
-      cols := make([]byte, 3)
-      if _, err := buff.Read(cols); err != nil {
-        return nil, fmt.Errorf("Card body read error: %w", err)
-      }
+		// Read `format` columns from the card body
+		for i := 0; i < endCol; {
+			// Get 3 bytes, and cast them to uint32
+			cols := make([]byte, 3)
+			if _, err := buff.Read(cols); err != nil {
+				return nil, fmt.Errorf("Card body read error: %w", err)
+			}
 
-      a := uint32(cols[0])
-      b := uint32(cols[1])
-      c := uint32(cols[2])
+			a := uint32(cols[0])
+			b := uint32(cols[1])
+			c := uint32(cols[2])
 
-      // Left column
-      left := (a << 4) | (b >> 4)
-      line.Write([]byte(string(ascii_code[left])))
-      i += 1
+			// Left column
+			left := (a << 4) | (b >> 4)
+			line.Write([]byte(string(ascii_code[left])))
+			i += 1
 
-      // Right column
-      right := ((b & 0017) << 8) | c
-      line.Write([]byte(string(ascii_code[right])))
-      i += 1
-    }
+			// Right column
+			right := ((b & 0017) << 8) | c
+			line.Write([]byte(string(ascii_code[right])))
+			i += 1
+		}
 
-    if format == 82 {
-      text.Write(line.Bytes()[1:line.Len() - 2])
-    } else {
-      text.ReadFrom(line)
-    }
+		if format == 82 {
+			text.Write(line.Bytes()[1 : line.Len()-2])
+		} else {
+			text.ReadFrom(line)
+		}
 
-    text.Write([]byte("\n"))
-  }
+		text.Write([]byte("\n"))
+	}
 
-  return text, nil
+	return text, nil
 }
--- cardcode.go
+++ cardcode.go
@@ -23,12 +23,17 @@ import (
 type Encoding int
 
 func (e Encoding) String() string {
-	switch (e) {
-	case Zero26comm: return "Zero26comm"
-	case Zero26ftn: return "Zero26ftn"
-	case Zero29ftn: return "Zero29ftn"
-	case EBCDIC: return "EBCDIC"
-	default: return "Unknown"
+	switch e {
+	case Zero26comm:
+		return "Zero26comm"
+	case Zero26ftn:
+		return "Zero26ftn"
+	case Zero29ftn:
+		return "Zero29ftn"
+	case EBCDIC:
+		return "EBCDIC"
+	default:
+		return "Unknown"
 	}
 }
 
@@ -51,11 +56,15 @@ func GetDecodingTable(e Encoding) []uint32 {
 }
 
 func EncodingFromString(s string) (Encoding, error) {
-	switch (s) {
-	case "026comm": return Zero26comm, nil
-	case "026ftn": return Zero26ftn, nil
-	case "029ftn": return Zero29ftn, nil
-	case "EBCDIC": return EBCDIC, nil
+	switch s {
+	case "026comm":
+		return Zero26comm, nil
+	case "026ftn":
+		return Zero26ftn, nil
+	case "029ftn":
+		return Zero29ftn, nil
+	case "EBCDIC":
+		return EBCDIC, nil
 	default:
 		return Zero26comm, fmt.Errorf("Invalid encoding '%s'", s)
 	}
@@ -64,15 +73,15 @@ func EncodingFromString(s string) (Encoding, error) {
 func init() {
 	for _, encoding := range []Encoding{Zero26comm, Zero26ftn, Zero29ftn, EBCDIC} {
 		decodings[encoding] = make([]uint32, 4096)
-	  for i := 0; i < len(decodings[encoding]); i += 1 {
-    	decodings[encoding][i] = '~'
-	  }
+		for i := 0; i < len(decodings[encoding]); i += 1 {
+			decodings[encoding][i] = '~'
+		}
 
 		start := ' '
 		end := '_'
 		table := zero_26_comm_code
 
-		switch (encoding) {
+		switch encoding {
 		case Zero26comm:
 			break
 		case Zero26ftn:
@@ -99,42 +108,42 @@ func init() {
 var inval uint32 = 00404
 
 var zero_26_comm_code = []uint32{
-	inval,inval,inval,inval,inval,inval,inval,inval, /* control */
-	inval,inval,inval,inval,inval,inval,inval,inval, /* chars   */
-	inval,inval,inval,inval,inval,inval,inval,inval, /* control */
-	inval,inval,inval,inval,inval,inval,inval,inval, /* chars   */
-	00000,inval,inval,00102,02102,01042,04000,inval, /*  !"#$%&' */
-	inval,inval,02042,inval,01102,02000,04102,01400, /* ()*+,-./ */
-	01000,00400,00200,00100,00040,00020,00010,00004, /* 01234567 */
-	00002,00001,inval,inval,04042,inval,inval,inval, /* 89:;<=>? */
-	00042,04400,04200,04100,04040,04020,04010,04004, /* @ABCDEFG */
-	04002,04001,02400,02200,02100,02040,02020,02010, /* HIJKLMNO */
-	02004,02002,02001,01200,01100,01040,01020,01010, /* PQRSTUVW */
-	01004,01002,01001,inval,inval,inval,inval,inval, /* XYZ[\]^_ */
-	inval,04400,04200,04100,04040,04020,04010,04004, /* `abcdefg */
-	04002,04001,02400,02200,02100,02040,02020,02010, /* hijklmno */
-	02004,02002,02001,01200,01100,01040,01020,01010, /* pqrstuvw */
-	01004,01002,01001,inval,inval,inval,inval,inval, /* xyz{|}~  */
+	inval, inval, inval, inval, inval, inval, inval, inval, /* control */
+	inval, inval, inval, inval, inval, inval, inval, inval, /* chars   */
+	inval, inval, inval, inval, inval, inval, inval, inval, /* control */
+	inval, inval, inval, inval, inval, inval, inval, inval, /* chars   */
+	00000, inval, inval, 00102, 02102, 01042, 04000, inval, /*  !"#$%&' */
+	inval, inval, 02042, inval, 01102, 02000, 04102, 01400, /* ()*+,-./ */
+	01000, 00400, 00200, 00100, 00040, 00020, 00010, 00004, /* 01234567 */
+	00002, 00001, inval, inval, 04042, inval, inval, inval, /* 89:;<=>? */
+	00042, 04400, 04200, 04100, 04040, 04020, 04010, 04004, /* @ABCDEFG */
+	04002, 04001, 02400, 02200, 02100, 02040, 02020, 02010, /* HIJKLMNO */
+	02004, 02002, 02001, 01200, 01100, 01040, 01020, 01010, /* PQRSTUVW */
+	01004, 01002, 01001, inval, inval, inval, inval, inval, /* XYZ[\]^_ */
+	inval, 04400, 04200, 04100, 04040, 04020, 04010, 04004, /* `abcdefg */
+	04002, 04001, 02400, 02200, 02100, 02040, 02020, 02010, /* hijklmno */
+	02004, 02002, 02001, 01200, 01100, 01040, 01020, 01010, /* pqrstuvw */
+	01004, 01002, 01001, inval, inval, inval, inval, inval, /* xyz{|}~  */
 }
 
 /* Bare bones 026 kepunch encodings */
 var zero_26_ftn_code = []uint32{
-	inval,inval,inval,inval,inval,inval,inval,inval, /* control */
-	inval,inval,inval,inval,inval,inval,inval,inval, /* chars   */
-	inval,inval,inval,inval,inval,inval,inval,inval, /* control */
-	inval,inval,inval,inval,inval,inval,inval,inval, /* chars   */
-	00000,inval,inval,inval,02102,inval,inval,00042, /*  !"#$%&' */
-	01042,04042,02042,04000,01102,02000,04102,01400, /* ()*+,-./ */
-	01000,00400,00200,00100,00040,00020,00010,00004, /* 01234567 */
-	00002,00001,inval,inval,inval,00102,inval,inval, /* 89:;<=>? */
-	inval,04400,04200,04100,04040,04020,04010,04004, /* @ABCDEFG */
-	04002,04001,02400,02200,02100,02040,02020,02010, /* HIJKLMNO */
-	02004,02002,02001,01200,01100,01040,01020,01010, /* PQRSTUVW */
-	01004,01002,01001,inval,inval,inval,inval,inval, /* XYZ[\]^_ */
-	inval,04400,04200,04100,04040,04020,04010,04004, /* `abcdefg */
-	04002,04001,02400,02200,02100,02040,02020,02010, /* hijklmno */
-	02004,02002,02001,01200,01100,01040,01020,01010, /* pqrstuvw */
-	01004,01002,01001,inval,inval,inval,inval,inval, /* xyz{|}~  */
+	inval, inval, inval, inval, inval, inval, inval, inval, /* control */
+	inval, inval, inval, inval, inval, inval, inval, inval, /* chars   */
+	inval, inval, inval, inval, inval, inval, inval, inval, /* control */
+	inval, inval, inval, inval, inval, inval, inval, inval, /* chars   */
+	00000, inval, inval, inval, 02102, inval, inval, 00042, /*  !"#$%&' */
+	01042, 04042, 02042, 04000, 01102, 02000, 04102, 01400, /* ()*+,-./ */
+	01000, 00400, 00200, 00100, 00040, 00020, 00010, 00004, /* 01234567 */
+	00002, 00001, inval, inval, inval, 00102, inval, inval, /* 89:;<=>? */
+	inval, 04400, 04200, 04100, 04040, 04020, 04010, 04004, /* @ABCDEFG */
+	04002, 04001, 02400, 02200, 02100, 02040, 02020, 02010, /* HIJKLMNO */
+	02004, 02002, 02001, 01200, 01100, 01040, 01020, 01010, /* PQRSTUVW */
+	01004, 01002, 01001, inval, inval, inval, inval, inval, /* XYZ[\]^_ */
+	inval, 04400, 04200, 04100, 04040, 04020, 04010, 04004, /* `abcdefg */
+	04002, 04001, 02400, 02200, 02100, 02040, 02020, 02010, /* hijklmno */
+	02004, 02002, 02001, 01200, 01100, 01040, 01020, 01010, /* pqrstuvw */
+	01004, 01002, 01001, inval, inval, inval, inval, inval, /* xyz{|}~  */
 }
 
 /* DEC's version of the IBM 029 kepunch encoding, (thus avoiding IBM's
@@ -143,22 +152,22 @@ var zero_26_ftn_code = []uint32{
    translate lower case to upper case.  As a result of this modification,
    inversion of this table should be done with care! */
 var zero_29_code = []uint32{
-	inval,inval,inval,inval,inval,inval,inval,inval, /* control */
-	inval,inval,inval,inval,inval,inval,inval,inval, /* chars   */
-	inval,inval,inval,inval,inval,inval,inval,inval, /* control */
-	inval,inval,inval,inval,inval,inval,inval,inval, /* chars   */
-	00000,02202,00006,00102,02102,01042,04000,00022, /*  !"#$%&' */
-	04022,02022,02042,04012,01102,02000,04102,01400, /* ()*+,-./ */
-	01000,00400,00200,00100,00040,00020,00010,00004, /* 01234567 */
-	00002,00001,00202,02012,04042,00012,01012,01006, /* 89:;<=>? */
-	00042,04400,04200,04100,04040,04020,04010,04004, /* @ABCDEFG */
-	04002,04001,02400,02200,02100,02040,02020,02010, /* HIJKLMNO */
-	02004,02002,02001,01200,01100,01040,01020,01010, /* PQRSTUVW */
-	01004,01002,01001,04202,02006,01202,04006,01022, /* XYZ[\]^_ */
-	inval,04400,04200,04100,04040,04020,04010,04004, /* `abcdefg */
-	04002,04001,02400,02200,02100,02040,02020,02010, /* hijklmno */
-	02004,02002,02001,01200,01100,01040,01020,01010, /* pqrstuvw */
-	01004,01002,01001,inval,inval,inval,inval,inval, /* xyz{|}~  */
+	inval, inval, inval, inval, inval, inval, inval, inval, /* control */
+	inval, inval, inval, inval, inval, inval, inval, inval, /* chars   */
+	inval, inval, inval, inval, inval, inval, inval, inval, /* control */
+	inval, inval, inval, inval, inval, inval, inval, inval, /* chars   */
+	00000, 02202, 00006, 00102, 02102, 01042, 04000, 00022, /*  !"#$%&' */
+	04022, 02022, 02042, 04012, 01102, 02000, 04102, 01400, /* ()*+,-./ */
+	01000, 00400, 00200, 00100, 00040, 00020, 00010, 00004, /* 01234567 */
+	00002, 00001, 00202, 02012, 04042, 00012, 01012, 01006, /* 89:;<=>? */
+	00042, 04400, 04200, 04100, 04040, 04020, 04010, 04004, /* @ABCDEFG */
+	04002, 04001, 02400, 02200, 02100, 02040, 02020, 02010, /* HIJKLMNO */
+	02004, 02002, 02001, 01200, 01100, 01040, 01020, 01010, /* PQRSTUVW */
+	01004, 01002, 01001, 04202, 02006, 01202, 04006, 01022, /* XYZ[\]^_ */
+	inval, 04400, 04200, 04100, 04040, 04020, 04010, 04004, /* `abcdefg */
+	04002, 04001, 02400, 02200, 02100, 02040, 02020, 02010, /* hijklmno */
+	02004, 02002, 02001, 01200, 01100, 01040, 01020, 01010, /* pqrstuvw */
+	01004, 01002, 01001, inval, inval, inval, inval, inval, /* xyz{|}~  */
 }
 
 /* FULL EBCDIC, from Appendix C of System 360 Programming by Alex Thomas,
@@ -166,20 +175,20 @@ var zero_29_code = []uint32{
    left compatable with DEC's 029 table.  Some control codes have been
    left out */
 var ebcdic_code = []uint32{
-	05403,inval,inval,inval,inval,inval,inval,inval, /* control */
-	02011,04021,01021,inval,04041,02021,inval,inval, /* chars   */
-	inval,inval,inval,inval,inval,inval,inval,inval, /* control */
-	inval,inval,inval,inval,01201,inval,inval,inval, /* chars   */
-	00000,02202,00006,00102,02102,01042,04000,00022, /*  !"#$%&' */
-	04022,02022,02042,04012,01102,02000,04102,01400, /* ()*+,-./ */
-	01000,00400,00200,00100,00040,00020,00010,00004, /* 01234567 */
-	00002,00001,00202,02012,04042,00012,01012,01006, /* 89:;<=>? */
-	00042,04400,04200,04100,04040,04020,04010,04004, /* @ABCDEFG */
-	04002,04001,02400,02200,02100,02040,02020,02010, /* HIJKLMNO */
-	02004,02002,02001,01200,01100,01040,01020,01010, /* PQRSTUVW */
-	01004,01002,01001,04202,02006,01202,04006,01022, /* XYZ[\]^_ */
-	inval,05400,05200,05100,05040,05020,05010,05004, /* `abcdefg */
-	05002,05001,06400,06200,06100,06040,06020,06010, /* hijklmno */
-	06004,06002,06001,03200,03100,03040,03020,03010, /* pqrstuvw */
-	03004,03002,03001,inval,inval,inval,inval,inval, /* xyz{|}~  */
+	05403, inval, inval, inval, inval, inval, inval, inval, /* control */
+	02011, 04021, 01021, inval, 04041, 02021, inval, inval, /* chars   */
+	inval, inval, inval, inval, inval, inval, inval, inval, /* control */
+	inval, inval, inval, inval, 01201, inval, inval, inval, /* chars   */
+	00000, 02202, 00006, 00102, 02102, 01042, 04000, 00022, /*  !"#$%&' */
+	04022, 02022, 02042, 04012, 01102, 02000, 04102, 01400, /* ()*+,-./ */
+	01000, 00400, 00200, 00100, 00040, 00020, 00010, 00004, /* 01234567 */
+	00002, 00001, 00202, 02012, 04042, 00012, 01012, 01006, /* 89:;<=>? */
+	00042, 04400, 04200, 04100, 04040, 04020, 04010, 04004, /* @ABCDEFG */
+	04002, 04001, 02400, 02200, 02100, 02040, 02020, 02010, /* HIJKLMNO */
+	02004, 02002, 02001, 01200, 01100, 01040, 01020, 01010, /* PQRSTUVW */
+	01004, 01002, 01001, 04202, 02006, 01202, 04006, 01022, /* XYZ[\]^_ */
+	inval, 05400, 05200, 05100, 05040, 05020, 05010, 05004, /* `abcdefg */
+	05002, 05001, 06400, 06200, 06100, 06040, 06020, 06010, /* hijklmno */
+	06004, 06002, 06001, 03200, 03100, 03040, 03020, 03010, /* pqrstuvw */
+	03004, 03002, 03001, inval, inval, inval, inval, inval, /* xyz{|}~  */
 }
